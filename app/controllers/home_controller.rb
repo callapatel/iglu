0c114ffa2c6aad2bee2dca96bf85874bd6e8e183
Calla <callapatel@gmail.com>
2015-01-30 13:25:56 -0800

    moved logic into the model:

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,45 +18,28 @@ class HomeController < ApplicationController
 
   def login
     token = User.find_by(id: session[:user_id]).token
+    this_user_atm = session[:user_id]
     @photos = Photo.all
-    response = HTTParty.get("https://api.instagram.com/v1/users/self/media/liked?access_token=#{token}&count=200&max_id=123456")
-
-
-    3.times do
-      @lovely = response.parsed_response["data"]
-      @lovely.each do |n|
-
-        welcome = n['images']['standard_resolution']['url']
-        if n['location']
-          location_name = n['location']['name']
-          location_latitude = n['location']['latitude']
-          location_longitude = n['location']['longitude']
-          Photo.find_or_create_by(url: "#{welcome}", user: session[:user_id], loc_name: "#{location_name}", lat: "#{location_latitude}" , long: "#{location_longitude}")
-        else
-          Photo.find_or_create_by(url: "#{welcome}", user: session[:user_id])    
-        end
-      end
-      @next = response.parsed_response['pagination']['next_url']
-      response = HTTParty.get("#{@next}")
-    end
-
+    Photo.api_call_to_db_update(token, this_user_atm)
+  #   response = HTTParty.get("https://api.instagram.com/v1/users/self/media/liked?access_token=#{token}&count=200&max_id=123456")
+  #   3.times do
+  #     @lovely = response.parsed_response["data"]
+  #     @lovely.each do |n|
+  #
+  #       welcome = n['images']['standard_resolution']['url']
+  #       if n['location']
+  #         location_name = n['location']['name']
+  #         location_latitude = n['location']['latitude']
+  #         location_longitude = n['location']['longitude']
+  #         Photo.find_or_create_by(url: "#{welcome}", user: session[:user_id], loc_name: "#{location_name}", lat: "#{location_latitude}" , long: "#{location_longitude}")
+  #       else
+  #         Photo.find_or_create_by(url: "#{welcome}", user: session[:user_id])
+  #       end
+  #     end
+  #     @next = response.parsed_response['pagination']['next_url']
+  #     response = HTTParty.get("#{@next}")
+  #   end
+  #
   end
-
-
-  private
-
-    def find_provider
-      @provider = Provider.find_by_name_and_user_id("instagram", session[:user_id])
-    end
-
-    def create_instagram_client
-      find_provider
-
-      Instagram.configure do |config|
-        config.client_id = ENV["INSTAGRAM_CLIENT_ID"]
-        config.client_secret = ENV["INSTAGRAM_CLIENT_SECRET"]
-        config.access_token =  @provider.token
-      end
-    end
 
 end
