131ba028d99bfdde9ed678206d81d0a47ede970b
Calla <callapatel@gmail.com>
2015-02-07 14:24:26 -0800

    album controller updated for modal inputs

--- app/controllers/album_controller.rb
+++ app/controllers/album_controller.rb
@@ -2,13 +2,23 @@ class AlbumController < ApplicationController
 
   def new
     #DO SOMETHING IF THE USER DOESNT HAVE ALBUM
+    @albumidfromparams = params[:album_id]
+    @albumnamefromparams = params[:album]["name"]
+    @this_here_now_person = current_user.id
+    @photo_id_from_params_modal = params[:photo]["id"]
 
-    if params[:album_id] == "" || params[:album_id].nil?
-      @gowithit = params[:album]["name"]
+    #so either one of these is nil from the user, either they chose from the dropped list or wrote it in and
+    #if they wrote it in its sent to the same field as if it were selected from the dropped list into the db
+    if @albumidfromparams == "" || @albumidfromparams.nil?
+      @gowithit = @albumnamefromparams
+      Album.create(a_name: @gowithit, uid: current_user.id)
     else
-      @gowithit = params[:album_id]
+      @gowithit = @albumidfromparams
+       Album.create(a_name: @gowithit, uid: current_user.id)
     end
-    AlbumPhoto.create(uid: current_user.id, photo_id: (params[:photo]["id"]), album_id: @gowithit)
+    #AlbumPhoto.create(uid: current_user.id, photo_id: (params[:photo]["id"]), album_id: @gowithit)
+    AlbumPhoto.create(uid: @this_here_now_person, photo_id: @photo_id_from_params_modal, album_id: @gowithit)
+
     redirect_to root_path
   end
 end
