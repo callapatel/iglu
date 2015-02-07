1d969a6fb3305dfc6decf310f90791ad9abac5a1
Calla <callapatel@gmail.com>
2015-02-06 16:40:36 -0800

    a little bit more readable code

--- app/assets/javascripts/modal_extra.js
+++ app/assets/javascripts/modal_extra.js
@@ -1,5 +1,5 @@
-$('button,[data-inventory-item-id]').click(function() {
-  var inventoryID = $(this).data('inventory-item-id');
-  $('#inventoryID').prop('value', inventoryID);
-  $('#addListItem').modal('show');
-});
+// $('button,[data-inventory-item-id]').click(function() {
+//   var inventoryID = $(this).data('id');
+//   $('#inventoryID').prop('value', inventoryID);
+//   $('#addListItem').modal('show');
+// });
--- app/controllers/album_controller.rb
+++ app/controllers/album_controller.rb
@@ -1,7 +1,6 @@
 class AlbumController < ApplicationController
 
   def new
-    puts params
     AlbumPhoto.create(uid: current_user.id, photo_id: (params[:photo]["id"]), album_id: (params[:album_id]))
     #redirect_to root_path
   end
