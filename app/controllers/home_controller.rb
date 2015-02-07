2a00b44d8ac9b2d44febe96519e6c25a02f4c6ca
Calla <callapatel@gmail.com>
2015-02-07 11:49:14 -0800

    new user gets api called then and there and changed to login method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,21 +5,27 @@ class HomeController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    @user = User.createuser_from_omniauth(auth_hash)
-    session[:user_id] = @user.id
+    @user = User.where(uid: auth_hash['uid']).first
+    if @user == nil
+      @user = User.createuser_from_omniauth(auth_hash)
+      session[:user_id] = @user.id
+      token = current_user.token
+      Photo.api_call_to_db_update(token, current_user.id)
+    else
+      session[:user_id] = @user.id
+    end
     redirect_to root_path
-
   end
 
   def login
     token = current_user.token #User.find_by(id: session[:user_id])
     this_user_atm = current_user.id #session[:user_id]
     this_user_atm.present? ? @photos = Photo.paginate(:page => params[:page], :per_page => 30) : Photo.api_call_to_db_update(token, this_user_atm)
-    if this_user_atm
-       @photos = Photo.paginate(:page => params[:page], :per_page => 30)
-    else
-      Photo.api_call_to_db_update(token, this_user_atm)
-    end
+    # if this_user_atm
+    # @photos = Photo.paginate(:page => params[:page], :per_page => 30)
+    # else
+    #   Photo.api_call_to_db_update(token, this_user_atm)
+    # end
   end
 
 end
