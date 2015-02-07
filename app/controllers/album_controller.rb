5b126749af20aaadb5d4a543d51883e6120742e7
Calla <callapatel@gmail.com>
2015-02-06 17:20:52 -0800

    some stuff is working in modal but now deleted elsewhere

--- app/controllers/album_controller.rb
+++ app/controllers/album_controller.rb
@@ -1,7 +1,7 @@
 class AlbumController < ApplicationController
 
   def new
-    AlbumPhoto.create(uid: current_user.id, photo_id: (params[:photo]["id"]), album_id: (params[:album_id]))
+    #AlbumPhoto.create(uid: current_user.id, photo_id: (params[:photo]["id"]), album_id: (params[:album_id]))
     #redirect_to root_path
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,8 @@ class HomeController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    @user = User.where(uid: auth_hash['uid']).first
-    if @user == nil
-      User.createuser_from_omniauth(auth_hash)
-      session[:user_id] = @user.id
-    else
-      session[:user_id] = @user.id
-    end
+    @user = User.createuser_from_omniauth(auth_hash)
+    session[:user_id] = @user.id
     redirect_to root_path
 
   end
@@ -20,11 +15,11 @@ class HomeController < ApplicationController
     token = current_user.token #User.find_by(id: session[:user_id])
     this_user_atm = current_user.id #session[:user_id]
     this_user_atm.present? ? @photos = Photo.paginate(:page => params[:page], :per_page => 30) : Photo.api_call_to_db_update(token, this_user_atm)
-    # if this_user_atm
-    #   @photos = Photo.paginate(:page => params[:page], :per_page => 30)
-    # else
+    if this_user_atm
+       @photos = Photo.paginate(:page => params[:page], :per_page => 30)
+    else
       Photo.api_call_to_db_update(token, this_user_atm)
-    #end
+    end
   end
 
 end
