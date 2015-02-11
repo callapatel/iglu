8c3e038417af6566942df1123cd6c6cb69e7a600
Calla <callapatel@gmail.com>
2015-02-11 13:58:01 -0800

    fonting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -25,8 +25,8 @@ class HomeController < ApplicationController
     if require_login
       if this_user_atm.present?
         @photos = Photo.paginate(:page => params[:page], :per_page => 30)
-        Photo.yelp
-        Photo.google
+        # Photo.yelp
+        # Photo.google
       else
         Photo.api_call_to_db_update(token, this_user_atm)
         Photo.google
