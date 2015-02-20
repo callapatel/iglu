aafc82d1f0b173a0fd5a9fa46dfcce4c4979e5d3
Calla <callapatel@gmail.com>
2015-02-20 11:20:48 -0800

    user nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :uid, :name, :token, presence: true
+  validates :uid, :token, presence: true
 
   def self.find_or_create_from_omniauth(auth_hash)
     # Find or create a user
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   def self.createuser_from_omniauth(auth_hash)
       @user = User.create(
-      name: auth_hash['info']['name'],
+      name: auth_hash['info']['nickname'],
       uid: auth_hash['uid'],
       token: auth_hash["credentials"]['token'],
       )
