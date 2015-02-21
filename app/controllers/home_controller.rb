fbdd6f00da26a0869e3dc4a0a0061df0412b7d70
Calla <callapatel@gmail.com>
2015-02-20 16:47:45 -0800

    messing around with api calls

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -32,8 +32,8 @@ class HomeController < ApplicationController
         @count= Album.where(uid: current_user.id).count
         @photos = Photo.where(user: current_user.id).paginate(:page => params[:page], :per_page => 30)
         Photo.api_call_to_db_update(token, this_user_atm)
-        #Photo.yelp
-        #Photo.google(this_user_atm)
+        Photo.yelp
+        Photo.google
       else
         Photo.api_call_to_db_update(token, this_user_atm)
         Photo.google
